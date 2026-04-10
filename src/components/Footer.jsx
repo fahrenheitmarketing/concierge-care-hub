466b1dc56bc00113f9b54f299fbf11b4b810cbee
Ricardo Casas <rcasas@fahrenheitmarketing.com>
2026-04-10 00:12:40 +0000

    File changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
           backgroundSize: '400px',
           backgroundRepeat: 'repeat',
           filter: 'invert(1)',
-          opacity: 0.2,
+          opacity: 0.04,
         }}
       />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
