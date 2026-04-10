316c3ed2c921eeabae7a07d55872eb4d10049657
Ricardo Casas <rcasas@fahrenheitmarketing.com>
2026-04-10 05:06:26 +0000

    File changes

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -150,9 +150,9 @@ export default function Services() {
         <div
           className="absolute inset-0"
           style={{
-            backgroundImage: `url('https://media.base44.com/images/public/69d83c5355efc3ff23bd0660/b9e0c751f_generated_image.png')`,
+            backgroundImage: `url('https://media.base44.com/images/public/69d83c5355efc3ff23bd0660/3b4d46ad9_generated_image.png')`,
             backgroundSize: 'cover',
-            backgroundPosition: 'center 30%',
+            backgroundPosition: 'center center',
           }}
         />
         <div className="absolute inset-0" style={{ backgroundColor: 'hsl(79 58% 35%)', opacity: 0.55 }} />
